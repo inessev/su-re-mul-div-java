da52faea497684e35c42f9026ccb4be048466c65
inessev <123724101+inessev@users.noreply.github.com>
2023-04-13 10:35:36 +0200

    prueba 1

---
+++ java.js
@@ -0,0 +1,10 @@
+
+    System.out.println("Escribe dos números para obtener el resultado de su suma, resta, multiplicación y división");
+    System.out.print("Dame el valor del primer número: ");
+      numero1 = scanner.nextDouble();
+    System.out.print("Dame el valor del segundo número: ");
+      numero2 = scanner.nextDouble();
+    System.out.println("La suma de estos números es " + (numero1 + numero2));
+    System.out.println("La resta de estos números es " + (numero1 - numero2));
+    System.out.println("La multiplicación de estos números es " + (numero1 * numero2));
+    System.out.println("La división de estos números es " + (numero1 / numero2));

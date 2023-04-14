4c1cd000f133143e7276b0cad281f487384ec245
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 18:01:26 +0200

    prueba final

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ number1 = parseInt(prompt('Escriba aquí el primer numero'));
 number2 = parseInt(prompt('Escriba aquí el segundo numero'));
 vsuma = number1 + number2;
 alert("La suma de "  + number1 +" y " + number2 +" es " +  +vsuma);
-alert ("La suma de " + number1 +" y " + number2 +" es " + ( number1 + number2));
 alert ("La resta de " + number1 +" y " + number2 +" es " + ( number1 - number2));
 alert ("La multiplicación de " + number1 +" y " + number2 +" es " + ( number1 * number2));
 alert ("La división de " + number1 +" y " + number2 +" es " + ( number1 / number2));

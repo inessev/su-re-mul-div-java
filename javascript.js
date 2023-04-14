28376b8c746ee74fc18a5b548d82f97efae0c47f
inessev <123724101+inessev@users.noreply.github.com>
2023-04-14 17:54:36 +0200

    prueba

--- javascript.js
+++ javascript.js
@@ -1,7 +1,20 @@
 alert ("Escriba dos números para obtener el resultado de su suma, resta, multiplicación y división");
-var number1= prompt("Escriba aquí el primer numero");
-var number2= prompt("Escriba aquí el segundo numero");
+var number1,number2,vsuma;
+n1 = parseInt(prompt('Escriba aquí el primer numero'));
+n2 = parseInt(prompt('Escriba aquí el segundo numero'));
+vsuma = n1 + n2;
+alert("La suma de "  + number1 +" y " + number2 +" es " +  +vsuma);
 alert ("La suma de " + number1 +" y " + number2 +" es " + ( number1 + number2));
 alert ("La resta de " + number1 +" y " + number2 +" es " + ( number1 - number2));
 alert ("La multiplicación de " + number1 +" y " + number2 +" es " + ( number1 * number2));
 alert ("La división de " + number1 +" y " + number2 +" es " + ( number1 / number2));
+
+<script>
+	var n1,n2,vsuma;
+	
+	n1 = parseInt(prompt('Ingresar numero 1'));
+	n2 = parseInt(prompt('Ingresar numero 2'));
+	
+	vsuma = n1 + n2;
+	document.write('La suma es '+vsuma);
+</script>
